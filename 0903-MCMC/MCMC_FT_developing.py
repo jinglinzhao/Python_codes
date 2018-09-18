dcdfab41b49ad4beae2d2ce64cc034e9ae6e4ef8
jinglinzhao <jinglinzhao@gmail.com>
2018-09-18 11:48:25 +1000

    developing

--- 0903-MCMC/MCMC_FT_developing.py
+++ 0903-MCMC/MCMC_FT_developing.py
@@ -10,16 +10,17 @@ import random
 from functions import gran_gen
 from functions import gaussian_smoothing
 
-
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 #==============================================================================
 # Setup
 #==============================================================================
 
-real_a      = 2.0
+real_a      = 20
 real_k      = 0.7
 real_phi    = 1.0
 
-N = 100
+N = 200
 amplitude1      = np.zeros(N)
 period1         = np.zeros(N)
 amplitude1_XY   = np.zeros(N)
@@ -28,8 +29,11 @@ amplitude1_ZX   = np.zeros(N)
 period1_ZX      = np.zeros(N)
 amplitude1_XYZ  = np.zeros(N)
 period1_XYZ     = np.zeros(N)
+amplitude1_LS   = np.zeros(N)
+period1_LS      = np.zeros(N)
 amplitude2      = np.zeros(N)
 period2         = np.zeros(N)
+FAIL            = 0
 
 yes1_a1     = 0     # FT correction: amplitude within 1 sigma
 yes1_k1     = 0     # FT correction: period within 1 sigma
@@ -141,7 +145,7 @@ for n in range(N):
     J   = np.array([RV_jitter[i] for i in x])
 
     # smoothing 
-    sl      = 2.5         # smoothing length
+    sl      = 2         # smoothing length
     XY      = gaussian_smoothing(x, X-Y, x, sl)
     ZX      = gaussian_smoothing(x, Z-X, x, sl)    
 
@@ -250,7 +254,7 @@ for n in range(N):
     sampler2 = emcee.EnsembleSampler(nwalkers, ndim, lnprob2, args=(x, X, yerr)) # Note that running with multiple threads takes three times the time
 
     print("Running first burn-in...")
-    pos2     = [[np.log(np.var(X)), np.log(1), 1., 0.] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
+    pos2     = [[np.log(np.std(X)), np.log(1), 1., 0.] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
     pos2, prob2, state2  = sampler2.run_mcmc(pos2, burn_in_1_step)
 
     print("Running second burn-in...")
@@ -337,30 +341,44 @@ for n in range(N):
     plt.savefig(new_dir + '5-Fit2.png')
     plt.close('all')
 
-
+    amplitude2[n]   = a2[0]
+    period2[n]      = k2[0]
  
     #==============================================================================
     # MCMC with jitter correction
     #==============================================================================
 
-    XYZ = 3.7784*XY+0.5*ZX
+    fit         = np.polyfit(XY, ZX, 1)
+    XYZ         = 0.5*fit[0]*XY+0.5*ZX
+    rms         = 10
+    chance_amp  = 0
+    chance_per  = 0
+
     for proto_jitter in [XY, ZX, XYZ]:
 
         if (proto_jitter == XY).all():
             print('# MCMC with XY jitter correction #')
             suffix = '_XY'
+
+            def lnprior(theta):
+                a, k, phi, m, b = theta
+                if (-5 < a < 5) and (-5 < k < 5) and (-2*np.pi < phi < 2*np.pi) and (0 < m < 3) and (-5. < b < 5.):
+                    return 0.0
+                return -np.inf
+
+        else:
+            def lnprior(theta):
+                a, k, phi, m, b = theta
+                if (-5 < a < 5) and (-5 < k < 5) and (-2*np.pi < phi < 2*np.pi) and (-3 < m < 3) and (-5. < b < 5.):
+                    return 0.0
+                return -np.inf
+
         if (proto_jitter == ZX).all():
             print('# MCMC with ZX jitter correction #')
             suffix = '_ZX'
         if (proto_jitter == XYZ).all():
             print('# MCMC with XYZ jitter correction #')
             suffix = '_XYZ'
-
-        def lnprior(theta):
-            a, k, phi, m, b = theta
-            if (-5 < a < 5) and (-5 < k < 5) and (-2*np.pi < phi < 2*np.pi) and (-3 < m < 3) and (-5. < b < 5.):
-                return 0.0
-            return -np.inf
 
         # As likelihood, we assume the chi-square. Note: we do not even need to normalize it.
         def lnlike(theta, x, y, yerr):
@@ -380,7 +398,7 @@ for n in range(N):
         sampler     = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x, X, yerr))
 
         print("Running first burn-in...")
-        pos         = [[np.log(np.var(X)), np.log(1), 1., 0., 0.] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
+        pos         = [[np.log(np.std(X)), np.log(1), 1., 0.1, 0.] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
         pos, prob, state  = sampler.run_mcmc(pos, burn_in_1_step)
 
         print("Running second burn-in...")
@@ -457,18 +475,6 @@ for n in range(N):
         # FT correction Plots
         #==============================================================================    
 
-        # fig = plt.figure()
-        # RV_diff0    = RV_IN0 - RV_FT0
-        # Jitter_pos0 = (RV_diff0 + b[0]) * m[0]
-        # # Jitter_pos0 = np.hstack((Jitter_pos0,Jitter_pos0))
-        # Jitter_pos  = (RV_diff + b[0]) * m[0]
-        # Jitter_in   = RV_jitter - RV_jitter[0]
-        # Jitter_in   = np.array([Jitter_in[i] for i in x%100])
-        # plt.plot(x, Jitter_in - np.mean(Jitter_in), '*', label='Jitter_in')
-        # plt.plot(x, Jitter_pos - np.mean(Jitter_pos), 'o', label='Jitter_pos')
-        # plt.legend()
-        # plt.savefig(new_dir + '4-Jitter_correction.png')
-
         fig = plt.figure()
         RV_pos  = a[0] * np.sin(x/100. * k[0] * 2. * np.pi + phi[0])
         RV_os   = a[0] * np.sin(t/100. * k[0] * 2. * np.pi + phi[0])
@@ -488,19 +494,29 @@ for n in range(N):
         frame2  = fig.add_axes((.1,.1,.8,.2))   
         frame2.axhline(color="gray", ls='--')
         res     = X - (Jitter_pos+RV_pos)
-        rms     = 10
-        if (np.sqrt(np.var(res)) < rms):
-            rms = np.sqrt(np.var(res))
-            amplitude1  = a[0]
-            period1     = k[0]
-        plt.errorbar(x/100, res, yerr=yerr, fmt=".k", capsize=0, alpha=0.5, label=r'rms$=%.2f$ m/s' %rms)
+        rms_new = np.std(res)
+
+        if (rms_new < rms):
+            rms = rms_new
+            chance_amp_new = gaussian(a[0], 2., 0.1) 
+            chance_per_new = gaussian(k[0], 0.7, 0.01)
+            if (chance_amp_new >= chance_amp) and (chance_per_new >= chance_per):     # indeed a better solution 
+                chance_amp_new = chance_amp
+                chance_per_new = chance_per
+            else:                   # not a better solution?
+                print('!Achtung!')
+                print([chance_amp_new/chance_amp, chance_per_new/chance_per])
+                FAIL = FAIL + 1
+            amplitude1[n]  = a[0]
+            period1[n]     = k[0]
+
+        plt.errorbar(x/100, res, yerr=yerr, fmt=".k", capsize=0, alpha=0.5, label=r'rms$=%.2f$ m/s' %rms_new)
         plt.xlim(0,4)
         plt.xlabel(r"$P_{rot}$")
         plt.ylabel("Residual [m/s]")
         plt.legend()
         plt.savefig(new_dir + '5-Fit1' + suffix + '.png')
         plt.close('all')
-
 
         # Statistics #
         if (proto_jitter == XY).all():
@@ -513,9 +529,126 @@ for n in range(N):
             amplitude1_XYZ[n]   = a[0]
             period1_XYZ[n]      = k[0]
 
-    amplitude2[n]   = a2[0]
-    period2[n]      = k2[0]
+    #==============================================================================
+    # MCMC with jitter correction 2
+    #==============================================================================
 
+    xmc = np.hstack((x,x)) #dumb
+    ymc = np.hstack((Y_s, Z_s)) #dumb
+    yerrmc = np.hstack((yerr,yerr)) #dumb
+
+    def lnprior(theta):
+        a, nu, phi, ky, offset_Y, offset_Z = theta
+        if (0 < a < 5) and (0 < nu < 5) and (-2*np.pi < phi < 2*np.pi) and (0 < ky < 1):
+            return 0.0
+        return -np.inf
+
+    # As likelihood, we assume the chi-square. Note: we do not even need to normalize it.
+    def lnlike(theta, xmc, ymc, yerrmc):
+        a, nu, phi, ky, offset_Y, offset_Z = theta
+        vy = ymc[0:60]
+        vz = ymc[60:120]
+        model_Y = ky*X - (ky-1)*a * np.sin(x/100. * nu * 2. * np.pi + phi) + offset_Y
+        kz = fit[0] * (1-ky) + 1
+        model_Z = kz*X - (kz-1)*a * np.sin(x/100. * nu * 2. * np.pi + phi) + offset_Z 
+        return -0.5*(np.sum( ((vy-model_Y)/yerr)**2.) + np.sum( ((vz-model_Z)/yerr)**2.))
+
+    def lnprob(theta, xmc, ymc, yerrmc):
+        lp = lnprior(theta)
+        if not np.isfinite(lp):
+            return -np.inf
+        return lp + lnlike(theta, xmc, ymc, yerrmc)    
+
+    #================================
+    import emcee
+    ndim        = 6
+    nwalkers    = 32
+    sampler     = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(xmc, ymc, yerrmc))
+
+    print("Running first burn-in...")
+    pos         = [[np.std(X), 1, 0, 0.8, 0, 0] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
+    pos, prob, _  = sampler.run_mcmc(pos, burn_in_1_step)
+
+    print("Running second burn-in...")
+    pos = [pos[np.argmax(prob)] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
+    pos, prob, _  = sampler.run_mcmc(pos, burn_in_2_step)
+
+    print("Running production...")
+    pos = [pos[np.argmax(prob)] + 1e-2*np.random.randn(ndim) for i in range(nwalkers)] 
+    sampler.run_mcmc(pos, production_step)
+
+    #==============================================================================
+    # Trace and corner plots 
+    #==============================================================================
+
+    fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
+    labels=[r"$A$", r"$\nu$", r"$\omega$", "ky", "offset_Y", "offset_Z"]
+    for i in range(ndim):
+        ax = axes[i]
+        ax.plot( np.rot90(sampler.chain[:, :, i], 3), "k", alpha=0.3)
+        ax.set_xlim(0, sampler.chain.shape[1])
+        ax.set_ylabel(labels[i])
+        ax.yaxis.set_label_coords(-0.1, 0.5)
+
+    axes[-1].set_xlabel("Step number");
+    plt.savefig(new_dir + '3-Trace1_LS.png')
+
+
+    import copy
+    log_samples         = sampler.chain[:, 3000:, :].reshape((-1, ndim))
+    real_samples        = copy.copy(log_samples)
+    # real_samples[:,1] = np.exp(real_samples[:,1])
+
+    import corner
+    fig = corner.corner(real_samples, labels=labels, truths=[real_a, real_k, real_phi, 100, 100, 100], quantiles=[0.16, 0.5, 0.84], show_titles=True, title_kwargs={"fontsize": 12})
+    plt.savefig(new_dir + '3-MCMC1_LS.png')
+
+    #==============================================================================
+    # Statistics
+    #==============================================================================    
+    a, nu, phi, ky, offset_Y, offset_Z = map(lambda v: np.array(v), zip(*np.percentile(real_samples, [50, 16, 84, 2.5, 97.5], axis=0)))
+
+    if (a[1] < real_a < a[2]) and (nu[1] < real_k < nu[2]):
+        print('Bingo - FT correction - 1 sigma')
+    if (a[3] < real_a < a[4]) and (nu[3] < real_k < nu[4]):
+        print('Bingo - FT correction - 2 sigma')
+
+    print(np.vstack((a, nu, phi, ky, offset_Y, offset_Z)))
+
+    amplitude1_LS[n]   = a[0]
+    period1_LS[n]      = k[0]
+
+    #==============================================================================
+    # FT correction Plots
+    #==============================================================================    
+
+    fig     = plt.figure()
+    # align both RV_pos and RV_os #
+    RV_pos  = a[0] * np.sin(x/100. * nu[0] * 2*np.pi + phi[0])
+    RV_os   = a[0] * np.sin(t/100. * nu[0] * 2*np.pi + phi[0])
+    RV_os   = RV_os - np.mean(RV_pos)
+    RV_pos  = RV_pos - np.mean(RV_pos)
+    frame1  = fig.add_axes((.1,.3,.8,.6))
+    frame1.axhline(color="gray", ls='--')
+    plt.errorbar(x/100, X, yerr=yerr, fmt="ok", capsize=0, label='Simulated RV', ecolor='blue', mfc='blue', mec='blue', alpha=0.5)
+    plt.xlim(0, 4)
+    plt.plot(x/100, RV_pos, 'gs')
+    plt.plot(t/100, RV_os, 'g--', label='Planet model')
+    plt.title('No correction')
+    plt.ylabel("RV [m/s]")
+    plt.legend()
+    frame1.set_xticklabels([])
+
+    frame2  = fig.add_axes((.1,.1,.8,.2))   
+    frame2.axhline(color="gray", ls='--')
+    rms     = np.sqrt(np.var(RV_pos - (X-np.mean(X))))
+    plt.errorbar(x/100, RV_pos - (X-np.mean(X)), yerr=yerr, fmt=".k", capsize=0, alpha=0.5, label=r'rms$=%.2f$ m/s' %rms)
+    plt.xlim(0, 4) 
+    plt.xlabel(r"$P_{rot}$")
+    plt.ylabel("Residual [m/s]")
+    plt.legend()
+    plt.savefig(new_dir + '5-Fit1_LS.png')
+    plt.close('all')
 
 #==============================================================================
 # Hostogram
@@ -535,71 +668,80 @@ if 0:
 mu, std = norm.fit(amplitude1_XY)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='XY')
+plotting = plt.plot(x_plot, p_plot, 'r', linewidth=2, label='XY')
 
 mu, std = norm.fit(amplitude1_ZX)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='ZX')
+plotting = plt.plot(x_plot, p_plot, 'b', linewidth=2, label='ZX')
 
 mu, std = norm.fit(amplitude1_XYZ)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='XYZ')
+plotting = plt.plot(x_plot, p_plot, 'g', linewidth=2, label='XYZ')
+
+mu, std = norm.fit(amplitude1_LS)
+x_plot = np.linspace(mu-3*std, mu+3*std, 100)
+p_plot = norm.pdf(x_plot, mu, std)
+plotting = plt.plot(x_plot, p_plot, 'gold', linewidth=2, label='LS')
 
 mu, std = norm.fit(amplitude2)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='No correction')
+plotting = plt.plot(x_plot, p_plot, 'k', linewidth=2, label='No correction')
 
 plt.legend()
 
 # plt.savefig('amp_dist.png')
 
-
 # bins  = np.linspace(bin_min, bin_max, 15)
-bins = 15
+bins = 20
 ax = plt.subplot(111)
 ax.axvline(x=real_a, color='k', ls='-.')
-plt.hist([amplitude1_XY, amplitude1_ZX, amplitude1_XYZ, amplitude2], bins, density=True, alpha=0.7)
+plt.hist([amplitude1_XY, amplitude1_ZX, amplitude1_XYZ, amplitude1_LS, amplitude2], color=['r', 'b', 'g', 'gold', 'k'], bins=bins, density=True, alpha=0.7)
 plt.xlabel('Amplitude [m/s]')
 plt.ylabel('Number density')
 plt.savefig('Histogram_1.png')
 plt.close('all')
 # plt.show()
 
-
 #####################
 
 mu, std = norm.fit(period1_XY)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='XY')
+plotting = plt.plot(x_plot, p_plot, 'r', linewidth=2, label='XY')
 
 mu, std = norm.fit(period1_ZX)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='ZX')
+plotting = plt.plot(x_plot, p_plot, 'b', linewidth=2, label='ZX')
 
 mu, std = norm.fit(period1_XYZ)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='XYZ')
+plotting = plt.plot(x_plot, p_plot, 'g', linewidth=2, label='XYZ')
+
+mu, std = norm.fit(period1_LS)
+x_plot = np.linspace(mu-3*std, mu+3*std, 100)
+p_plot = norm.pdf(x_plot, mu, std)
+plotting = plt.plot(x_plot, p_plot, 'gold', linewidth=2, label='LS')
 
 mu, std = norm.fit(period2)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='No correction')
+plotting = plt.plot(x_plot, p_plot, 'k', linewidth=2, label='No correction')
 
+plt.legend()
 # plt.savefig('amp_dist.png')
 
 
 # bins  = np.linspace(bin_min, bin_max, 15)
-bins = 15
+bins = 40
 ax = plt.subplot(111)
 ax.axvline(x=real_k, color='k', ls='-.')
-plt.hist([period1_XY, period1_ZX, period1_XYZ, period2], bins, density=True, alpha=0.7)
-plt.xlabel('nu')
+plt.hist([period1_XY, period1_ZX, period1_XYZ, period1_LS, period2], color=['r', 'b', 'g', 'gold', 'k'], bins=bins, density=True, alpha=0.7)
+plt.xlabel(r'$\nu_{orbital}$ / $\nu_{rot}$')
 plt.ylabel('Number density')
 plt.savefig('Histogram_2.png')
 plt.close('all')
@@ -635,29 +777,23 @@ if 0:
     plt.savefig('Histogram_2.png')
     # plt.show()
 
-# print('FT correction:')
-# print(yes1_both1/N, yes1_both2/N)
-# print('No correction')
-# print(yes2_both1/N, yes2_both2/N)
-# Performance = np.vstack((np.hstack((yes1_both1/N, yes1_both2/N)), np.hstack((yes2_both1/N, yes2_both2/N))))
-# np.savetxt('Performance.txt', Performance, fmt='%.2f')
 
 mu, std = norm.fit(amplitude1)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='Jitter correction')
+plotting = plt.plot(x_plot, p_plot, 'r', linewidth=2, label='Jitter correction')
 
 mu, std = norm.fit(amplitude2)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='No correction')
+plotting = plt.plot(x_plot, p_plot, 'k', linewidth=2, label='No correction', alpha = 0.5)
 
 plt.legend()
 
-bins = 15
+bins = 20
 ax = plt.subplot(111)
 ax.axvline(x=real_a, color='k', ls='-.')
-plt.hist([amplitude1, amplitude2], bins, density=True, alpha=0.7)
+plt.hist([amplitude1, amplitude2], color=['r', 'k'], bins=bins, density=True, alpha=0.7)
 plt.xlabel('Amplitude [m/s]')
 plt.ylabel('Number density')
 plt.savefig('Histogram_new1.png')
@@ -667,21 +803,38 @@ plt.close('all')
 mu, std = norm.fit(period1)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='Jitter correction')
+plotting = plt.plot(x_plot, p_plot, 'r', linewidth=2, label='Jitter correction')
 
 mu, std = norm.fit(period2)
 x_plot = np.linspace(mu-3*std, mu+3*std, 100)
 p_plot = norm.pdf(x_plot, mu, std)
-plotting = plt.plot(x_plot, p_plot, linewidth=2, label='No correction')
+plotting = plt.plot(x_plot, p_plot, 'k', linewidth=2, label='No correction', alpha = 0.5)
 
-bins = 15
+plt.legend()
+
+bins = 20
 ax = plt.subplot(111)
 ax.axvline(x=real_k, color='k', ls='-.')
-plt.hist([period1, period2], bins, density=True, alpha=0.7)
-plt.xlabel('nu')
+plt.hist([period1, period2], color=['r', 'k'], bins=bins, density=True, alpha=0.7)
+plt.xlabel(r'$\nu_{orb}$ / $\nu_{rot}$')
 plt.ylabel('Number density')
 plt.savefig('Histogram_new2.png')
 plt.close('all')
+
+array = np.arange(200)
+idx_a1 = (1.9 < amplitude1) * (amplitude1< 2.1)
+idx_a2 = (1.9 < amplitude2) * (amplitude2< 2.1)
+idx_p1 = (real_k-0.1 < period1) * (period1< real_k+0.1)
+idx_p2 = (real_k-0.1 < period2) * (period2< real_k+0.1)
+print(sum(idx_a1))
+print(sum(idx_a2))
+print(sum(idx_p1))
+print(sum(idx_p2))
+
+print(sum(idx_a1*idx_p1))
+print(sum(idx_a2*idx_p2))
+
+
 
 #==============================================================================
 # Ende
@@ -690,4 +843,4 @@ plt.close('all')
 time1 = time.time()
 print('\nRuntime = %.2f seconds' %(time1 - time0))
 
-
+print(FAIL)
