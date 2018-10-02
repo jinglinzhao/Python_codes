56b3d214438f6c9dcb874ec6392ec4455d3e8075
jinglinzhao <jinglinzhao@gmail.com>
2018-10-02 20:06:21 +1000

    developping

--- 0928-tau_ceti/tau_ceti.py
+++ 0928-tau_ceti/tau_ceti.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from rv import solve_kep_eqn
 # from celerite.modeling import Model
+from functions import gaussian_smoothing
 
 #==============================================================================
 # Import data 
@@ -14,6 +15,7 @@ yerr 	= np.loadtxt('RV_noise.dat') #m/s
 
 jitter_raw  = np.loadtxt('jitter_raw.txt')
 jitter_raw	= jitter_raw[idx]
+
 # jitter_smooth = np.loadtxt('jitter_smooth.txt')
 
 # import time
@@ -26,40 +28,62 @@ jitter_raw	= jitter_raw[idx]
 
 plt.figure()
 plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+plt.errorbar(x, y-YY, yerr=yerr, fmt=".r", capsize=0)
 plt.ylabel("RV [m/s]")
 plt.xlabel("Time [d]")
-plt.savefig('HD10700-1-RV.png')
+# plt.savefig('HD10700-1-RV.png')
 plt.show()
 
-54099
+# 54099
 
-54422: 4 day period?
+# 54422: 4 day period?
 
 
-XX  = np.loadtxt('XX.txt')
+
+# XX  = np.loadtxt('XX.txt')
 YY  = np.loadtxt('YY.txt')
 ZZ  = np.loadtxt('ZZ.txt')
 
+sl      = 0.5         # smoothing length
+XY      = gaussian_smoothing(x, y-YY, x, sl)
+XX 	 	= gaussian_smoothing(x, y, x, sl)
+plt.figure()
+# plt.errorbar(x, XX, yerr=yerr, fmt=".k", capsize=0)
+# plt.errorbar(x, XY, yerr=yerr, fmt=".r", capsize=0)
+plt.plot(x, XX, '.k')
+plt.plot(x, XY, '.r')
+plt.ylabel("RV [m/s]")
+plt.xlabel("Time [d]")
+# plt.savefig('HD10700-1-RV.png')
+plt.show()
+
+# idx = (x > 53500) & (x < 53800)
+idx = (x > 54250) & (x < 54500)
+
+x = x[idx]
+XX = XX[idx]
+XY = XY[idx]
+yerr = yerr[idx]
 
 #==============================================================================
 # Lomb-Scargle periodogram 
 #==============================================================================
 from astropy.stats import LombScargle
-min_f   = 1/5000
+min_f   = 1/100
 max_f   = 1
 spp     = 10
 
-frequency0, power0 = LombScargle(x, y, yerr).autopower(minimum_frequency=min_f,
+frequency0, power0 = LombScargle(x, XX, yerr).autopower(minimum_frequency=min_f,
                                                         maximum_frequency=max_f,
                                                         samples_per_peak=spp)
 
-frequency1, power1 = LombScargle(x, y-YY, yerr).autopower(minimum_frequency=min_f,
+frequency1, power1 = LombScargle(x, XY, yerr).autopower(minimum_frequency=min_f,
                                                             maximum_frequency=max_f,
                                                             samples_per_peak=spp)
 
 plt.figure()
 ax = plt.subplot(111)
-ax.set_xscale('log')
+# ax.set_xscale('log')
 ax.axhline(y=0, color='k')
 # ax.axvline(x=394, color='k')
 # ax.axvline(x=843, color='k')
@@ -70,4 +94,5 @@ plt.title('Lomb-Scargle Periodogram')
 plt.xlabel("Period [d]")
 plt.ylabel("Power")
 plt.legend()
-plt.savefig('HD10700-0-Periodogram.png')
+# plt.savefig('HD10700-0-Periodogram.png')
+plt.show()
