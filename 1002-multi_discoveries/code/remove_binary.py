9db1d42d6ce7b26cb63d79c04a1e4f4b7983aae9
jinglinzhao <jinglinzhao@gmail.com>
2018-11-29 17:08:35 +1100

    beautify the code

--- 1002-multi_discoveries/code/remove_binary.py
+++ 1002-multi_discoveries/code/remove_binary.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#==============================================================================
+# import data
+#==============================================================================
 
 DIR = '/Volumes/DataSSD/Python_codes/1002-multi_discoveries/data/HD128621/plot/'
 
@@ -16,23 +19,24 @@ t2011 		= np.loadtxt(DIR + '2011/plot_t.txt')
 y2011 		= np.loadtxt(DIR + '2011/plot_RV_HARPS.txt')
 yerr2011 	= np.loadtxt(DIR + '2011/plot_yerr.txt')
 
-t 		= np.hstack([t2009, t2010, t2011])
-y 		= np.hstack([y2009, y2010, y2011]) * 1000
-y 		= y - np.mean(y)
+t 		= np.hstack([t2009, t2010, t2011]) + 0.5
+RV_HARPS= np.hstack([y2009, y2010, y2011]) * 1000
 yerr 	= np.hstack([yerr2009, yerr2010, yerr2011])
 
-# plt.errorbar(t, y, yerr=yerr, fmt=".k", alpha=0.2)
-# plt.show()
-
+#==============================================================================
+# Fitting
+#==============================================================================
 
 from numpy.polynomial import polynomial as P
-c, stats    = P.polyfit(t, y, 2, full=True, w = 1/yerr**2)
-x_fit       = np.linspace(min(t-1), max(t+1), 10000)
-y_fit       = P.polyval(x_fit, c)
-# plt.errorbar(t, y, yerr=yerr, fmt=".k", capsize=0, alpha=0.2)
-# plt.plot(x_fit, y_fit)
-# plt.show()
+c, stats    = P.polyfit(t, RV_HARPS, 2, full=True, w = 1/yerr**2)
 
-y_plot = P.polyval(t, c)
-plt.errorbar(t, y-y_plot, yerr=yerr, fmt=".k", capsize=0, alpha=0.2)
+if 0: # visualize the fitting 
+	x_fit       = np.linspace(min(t-1), max(t+1), 10000)
+	y_fit       = P.polyval(x_fit, c)
+	plt.errorbar(t, RV_HARPS, yerr=yerr, fmt=".k", capsize=0, alpha=0.2)
+	plt.plot(x_fit, y_fit)
+	plt.show()
+
+y_fitt = P.polyval(t, c)
+plt.errorbar(t, RV_HARPS-y_fitt, yerr=yerr, fmt=".k", capsize=0, alpha=0.2)
 plt.show()
