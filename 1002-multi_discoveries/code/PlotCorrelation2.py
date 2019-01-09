83b1e382f5f8b68e260f06b7f5b61639ab6a43c2
jinglinzhao <jinglinzhao@gmail.com>
2019-01-09 14:52:11 +1100

    change name

--- 1002-multi_discoveries/code/PlotCorrelation2.py
+++ 1002-multi_discoveries/code/PlotCorrelation2.py
@@ -10,24 +10,31 @@ from scipy import stats
 # Import data 
 #==============================================================================
 
-directory 	= '/Volumes/DataSSD/MATLAB_codes/0615-FT-HD189733/'
+# directory 	= '/Volumes/DataSSD/MATLAB_codes/0615-FT-HD189733/'
+star 		= 'HD103720'
+directory 	= '/Volumes/DataSSD/MATLAB_codes/0816-FT-multiple_stars/' + star + '/'
 x 			= np.loadtxt(directory + 'GG.txt')
 y 			= np.loadtxt(directory + 'YY.txt')
 z 			= np.loadtxt(directory + 'ZZ.txt')
 bi 			= np.loadtxt(directory + 'BI.txt')
 xy 			= x - y
-# xy  		= np.loadtxt(directory + 'XY.txt')
 zx   		= z - x 
 
 #==============================================================================
 # Correlation 1
 #==============================================================================
 
-left  = 0.06  # the left side of the subplots of the figure
+if star == 'HD103720':
+	left = 0.07
+else:
+	left  = 0.06  # the left side of the subplots of the figure
 right = 0.97    # the right side of the subplots of the figure
 bottom = 0.15   # the bottom of the subplots of the figure
 top = 0.9      # the top of the subplots of the figure
-wspace = 0.4   # the amount of width reserved for blank space between subplots
+if star == 'HD103720':
+	wspace = 0.5
+else: 
+	wspace = 0.4   # the amount of width reserved for blank space between subplots
 hspace = 0.2   # the amount of height reserved for white space between subplots
 alpha = 0.5
 markersize = 20
@@ -44,7 +51,7 @@ spacing = (max(x) - min(x)) * 0.1
 plt.xlim(min(x)-spacing, max(x)+spacing)
 
 plt.subplot(152)
-plt.plot(x, y, '.k', markersize=markersize, alpha=alpha)
+plt.plot(x, z, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$RV_{FT,H}$ [m/s]')   
 plt.xlim(min(x)-spacing, max(x)+spacing)
@@ -53,10 +60,11 @@ plt.subplot(153)
 plt.plot(x, xy, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$\Delta RV_L$ [m/s]')
-plt.title('HD 189733 (companions not removed)')
+# plt.title('HD 189733 (companions not removed)')
+plt.title('HD ' + star[2:] + ' (planet not removed)')
 plt.xlim(min(x)-spacing, max(x)+spacing)
 r, p = stats.pearsonr(x, xy)
-plt.text(min(x), 0.9*max(xy)+0.1*min(xy), 'R={:.2f}'.format(r), fontsize=20)
+plt.text(min(x), 0.95*max(xy)+0.05*min(xy), 'R={:.3f}'.format(r), fontsize=20)
 
 plt.subplot(154)
 plt.plot(x, zx, '.k', markersize=markersize, alpha=alpha)
@@ -64,7 +72,7 @@ plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$\Delta RV_H$ [m/s]')
 plt.xlim(min(x)-spacing, max(x)+spacing)
 r, p = stats.pearsonr(x, zx)
-plt.text(min(x), 0.9*max(zx)+0.1*min(zx), 'R={:.2f}'.format(r), fontsize=20)
+plt.text(min(x), 0.95*max(zx)+0.05*min(zx), 'R={:.3f}'.format(r), fontsize=20)
 
 plt.subplot(155)
 plt.plot(xy, zx, '.k', markersize=markersize, alpha=alpha)
@@ -72,7 +80,7 @@ plt.xlabel(r'$\Delta RV_L$ [m/s]')
 plt.ylabel(r'$\Delta RV_H$ [m/s]')     
 spacing_xy = (max(xy) - min(xy)) * 0.1
 plt.xlim(min(xy)-spacing_xy, max(xy)+spacing_xy)
-plt.savefig('Correlation_HD189733_1.png')   
+plt.savefig('../output/Correlation_' + star + '_1.png')
 plt.show()
 
 #==============================================================================
@@ -87,36 +95,55 @@ plt.subplot(151)
 plt.plot(x-bi, y-bi, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$RV_{FT,L}$ [m/s]')    
-plt.xlim(min(x)-spacing, max(x)+spacing)
+if star == 'HD103720':
+	pass
+else:
+	plt.xlim(min(x)-spacing, max(x)+spacing)
+
 
 plt.subplot(152)
-plt.plot(x-bi, y-bi, '.k', markersize=markersize, alpha=alpha)
+plt.plot(x-bi, z-bi, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
-plt.ylabel(r'$RV_{FT,H}$ [m/s]')        
-plt.xlim(min(x)-spacing, max(x)+spacing)
+plt.ylabel(r'$RV_{FT,H}$ [m/s]')
+if star == 'HD103720':
+	pass
+else:
+	plt.xlim(min(x)-spacing, max(x)+spacing)
+
 
 plt.subplot(153)
 plt.plot(x-bi, xy, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$\Delta RV_L$ [m/s]')
-plt.title('HD 189733 (companions removed)')
-plt.xlim(min(x)-spacing, max(x)+spacing)
-np.corrcoef(x-bi, xy)
 r, p = stats.pearsonr(x-bi, xy)
-plt.text(min(x), 0.9*max(xy)+0.1*min(xy), 'R={:.2f}'.format(r), fontsize=20)
+if star == 'HD103720':
+	plt.title('HD ' + star[2:] + ' (planet removed)')
+	# plt.xlim(min(x-bi)-spacing, max(x-bi)+spacing)
+	plt.text(min(x-bi), 0.95*max(xy)+0.05*min(xy), 'R={:.3f}'.format(r), fontsize=20)
+else: 
+	plt.title('HD 189733 (companions removed)')
+	plt.xlim(min(x)-spacing, max(x)+spacing)
+	plt.text(min(x), 0.95*max(xy)+0.05*min(xy), 'R={:.3f}'.format(r), fontsize=20)
+# np.corrcoef(x-bi, xy)
+
 
 plt.subplot(154)
 plt.plot(x-bi, zx, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$RV_{HARPS}$ [m/s]')
 plt.ylabel(r'$\Delta RV_H$ [m/s]')
-plt.xlim(min(x)-spacing, max(x)+spacing)
 r, p = stats.pearsonr(x-bi, zx)
-plt.text(min(x), 0.9*max(zx)+0.1*min(zx), 'R={:.2f}'.format(r), fontsize=20)
+if star == 'HD103720':
+	# plt.xlim(min(x-bi)-spacing, max(x-bi)+spacing)
+	plt.text(min(x-bi), 0.95*max(zx)+0.05*min(zx), 'R={:.3f}'.format(r), fontsize=20)
+else: 
+	plt.xlim(min(x)-spacing, max(x)+spacing)
+	plt.text(min(x), 0.95*max(zx)+0.05*min(zx), 'R={:.3f}'.format(r), fontsize=20)
+
 
 plt.subplot(155)
 plt.plot(xy, zx, '.k', markersize=markersize, alpha=alpha)
 plt.xlabel(r'$\Delta RV_L$ [m/s]')    
 plt.ylabel(r'$\Delta RV_H$ [m/s]')     
 plt.xlim(min(xy)-spacing_xy, max(xy)+spacing_xy)
-plt.savefig('Correlation_HD189733_2.png')   
+plt.savefig('../output/Correlation_' + star + '_2.png')
 plt.show()
